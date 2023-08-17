5b721c4ff9955800e82c92b5febb3ff673bbbfe9
chae0 <limchae098@gmail.com>
2023-08-17 10:28:13 +0900

    ADD : init structure

---
+++ src/test/java/com/sangbu3jo/elephant/ElephantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangbu3jo.elephant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElephantApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
