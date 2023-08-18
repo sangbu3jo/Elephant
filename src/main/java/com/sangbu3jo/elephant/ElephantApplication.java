c4e9ddb5fcfa54825a1c040fc706c5c75b1542d4
chae0 <limchae098@gmail.com>
2023-08-18 12:53:11 +0900

    test

--- src/main/java/com/sangbu3jo/elephant/ElephantApplication.java
+++ src/main/java/com/sangbu3jo/elephant/ElephantApplication.java
@@ -3,6 +3,7 @@ package com.sangbu3jo.elephant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ElephantApplication {
 
