e5b4c5032eabae4f0cd22c1a41c54100cebc43aa
azuressu <martina0130@naver.com>
2023-08-22 12:49:21 +0900

    #8 FEAT : 카드 담당자 기능 수정

---
+++ src/main/java/com/sangbu3jo/elephant/card/dto/CardUserRequestDto.java
@@ -0,0 +1,13 @@
+package com.sangbu3jo.elephant.card.dto;
+
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class CardUserRequestDto {
+
+    private List<String> cardusernames;
+
+}
