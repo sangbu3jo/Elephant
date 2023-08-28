e3df59739ba4fd0aae1deb4dd9e9c8bbe22b8b06
chae0 <limchae098@gmail.com>
2023-08-28 22:54:48 +0900

    FEAT : 사용자 및 관리자 페이지 상단바 추가, CSS 수정 필요
    관리자 페이지 css
    1. 게시글 5개씩 출력하도록 변경 요망
    2. 출력부분 가운데 정렬 필요

--- src/main/java/com/sangbu3jo/elephant/admin/controller/AdminViewController.java
+++ src/main/java/com/sangbu3jo/elephant/admin/controller/AdminViewController.java
@@ -1,6 +1,10 @@
 package com.sangbu3jo.elephant.admin.controller;
 
+import com.sangbu3jo.elephant.security.UserDetailsImpl;
+import com.sangbu3jo.elephant.users.entity.UserRoleEnum;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,19 +14,33 @@ public class AdminViewController {
 
     // 관리자페이지
     @GetMapping("/admins")
-    public String backAdminsPage(){
+    public String backAdminsPage(Model model,
+        @AuthenticationPrincipal UserDetailsImpl userDetails){
+        Boolean admin = false;
+        if (userDetails.getUser().getRole().equals(UserRoleEnum.ADMIN)) {
+            admin = true;
+        }
+        model.addAttribute("admin", admin);
         return "backAdmins";
     }
 
+
     // 사용자 정보관리 페이지
     @GetMapping("/users")
-    public String backUsersPage(){
+    public String backUsersPage(Model model,
+        @AuthenticationPrincipal UserDetailsImpl userDetails){
+        Boolean admin = false;
+        if (userDetails.getUser().getRole().equals(UserRoleEnum.ADMIN)) {
+            admin = true;
+        }
+        model.addAttribute("admin", admin);
         return "backUsers";
     }
 
     // notification 테스트
     @GetMapping("/notification")
-    public String notification(){
+    public String notification() {
         return "notification";
     }
+
 }
