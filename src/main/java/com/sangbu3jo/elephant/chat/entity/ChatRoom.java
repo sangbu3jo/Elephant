b4e98b32a7cee0e33f6d5b74fd1fc019006fb25d
azuressu <martina0130@naver.com>
2023-08-28 10:10:54 +0900

    FEAT : 1차 수정

--- src/main/java/com/sangbu3jo/elephant/chat/entity/ChatRoom.java
+++ src/main/java/com/sangbu3jo/elephant/chat/entity/ChatRoom.java
@@ -20,18 +20,18 @@ public class ChatRoom {
     @OneToOne
     private Board board;
 
-//    @ElementCollection
-//    private List<String> users = new ArrayList<>();
+    @ElementCollection(fetch = FetchType.EAGER) // 다른 방법을 생각해보자
+    private List<String> users = new ArrayList<>();
 
     public ChatRoom(Long id, Board board){
         this.id = id;
         this.board = board;
     }
 
-//    public void addUser(String username) {
-//        this.users.add(username);
-//    }
-//
+    public void addUser(String username) {
+        this.users.add(username);
+    }
+
 //    public void removeUser(String username) {
 //        this.users.remove(username);
 //    }
