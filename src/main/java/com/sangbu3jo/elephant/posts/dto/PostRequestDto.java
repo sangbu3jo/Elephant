51cdb32be70fcb23692a35961ab6662ca2719d14
gunwook <taeme0912@naver.com>
2023-08-29 15:23:24 +0900

    REFACTOR : html 알림부분 수정, 컨트롤러 경로수정

--- src/main/java/com/sangbu3jo/elephant/posts/dto/PostRequestDto.java
+++ src/main/java/com/sangbu3jo/elephant/posts/dto/PostRequestDto.java
@@ -1,5 +1,6 @@
 package com.sangbu3jo.elephant.posts.dto;
 
+import com.sangbu3jo.elephant.posts.entity.Category;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
