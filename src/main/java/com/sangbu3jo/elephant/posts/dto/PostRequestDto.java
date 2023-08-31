59bfbcb52fd7e2f9febf25863cedebf1ff0e1b62
Choo Hyunjoong <hjchoo19950327@gmail.com>
2023-08-31 20:02:40 +0900

    [ADD]: @Valid NotEmpty 추가

--- src/main/java/com/sangbu3jo/elephant/posts/dto/PostRequestDto.java
+++ src/main/java/com/sangbu3jo/elephant/posts/dto/PostRequestDto.java
@@ -2,6 +2,7 @@ package com.sangbu3jo.elephant.posts.dto;
 
 import com.sangbu3jo.elephant.posts.entity.Category;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ public class PostRequestDto {
 
     @NotEmpty(message = "제목을 입력해주세요.")
     private String title;
+
     @NotEmpty(message = "게시글을 입력해주세요.")
     private String content;
 
