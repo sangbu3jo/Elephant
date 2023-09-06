659aa80c492a8390be5cb45e04d9e22d3ef1a146
azuressu <martina0130@naver.com>
2023-09-06 20:42:20 +0900

    FIX : Test코드 오류 수정

--- src/test/java/com/sangbu3jo/elephant/ElephantApplicationTests.java
+++ src/test/java/com/sangbu3jo/elephant/ElephantApplicationTests.java
@@ -1,13 +1,8 @@
 package com.sangbu3jo.elephant;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ElephantApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
 
 }
