7db9c8205bf484672e7a2c34c64b26a7b987d736
azuressu <martina0130@naver.com>
2023-09-11 15:12:05 +0900

    FIX : 카드 오류 수정

--- src/main/java/com/sangbu3jo/elephant/card/dto/CardUserRequestDto.java
+++ src/main/java/com/sangbu3jo/elephant/card/dto/CardUserRequestDto.java
@@ -1,15 +1,15 @@
 package com.sangbu3jo.elephant.card.dto;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CardUserRequestDto {
 
     private List<String> cardusernames;
--- src/main/java/com/sangbu3jo/elephant/cardcomment/dto/CardcommentRequestDto.java
+++ src/main/java/com/sangbu3jo/elephant/cardcomment/dto/CardcommentRequestDto.java
@@ -1,12 +1,12 @@
 package com.sangbu3jo.elephant.cardcomment.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CardcommentRequestDto {
 
     private String content;
