a8ee895e66fb9cd6479a17b1def45c9cf7e3bacb
chae0 <limchae098@gmail.com>
2023-09-14 12:37:06 +0900

    docs: create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+REPOSITORY=/home/ubuntu/app
+cd $REPOSITORY
+
+APP_NAME=app
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ] # CURRENT_PID 값이 없는지 여부 판
+then # $CURRENT_PID가 비어 있을 때 실행되는 부분
+  echo "> 종료할것 없음."
+else # $CURRENT_PID가 비어 있지 않을 때 실행되는 부분
+  echo "> kill -9 $CURRENT_PID" 
+  kill -9 $CURRENT_PID # 해당 프로세스 강제 종료
+  sleep 5 # 5초 대기
+fi
+
+echo "> $JAR_PATH 배포"
+# nohup을 사용하여 백그라운드에서 Java JAR 파일 실행
+# 출력 및 오류를 무시하고 표준 입력을 /dev/null로 지정
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /del/null &
